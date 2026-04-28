eedf92f093ac844528437684a765d6e8c3a26ddc
Gabriel Ken Kudo de Lima <gabkenkudo@gmail.com>
2026-04-28 14:41:01 -0300

    Máquina acumuladora de respostas atualizada.
    Atualização da máquina acumuladora de respostas:
    - conserto de erros técnicos importantes;
    - Algumas novas respostas;
    - Print em console da versão da máquina e
    - Print para melhora na visualização em console.

--- Mini ia friend.py
+++ Mini ia friend.py
@@ -22,6 +22,7 @@ def coletor():
     conteudo: str = ''
     frase: str = ''
     resposta: str = ''
+    printe: str = '        print(Resposta)'
 
     arq = 'novas_respostas.txt'
     dir = 'C:\\tmp\\'
@@ -29,7 +30,7 @@ def coletor():
     Resposta = str(input("Não tenho resposta para sua pergunta. Ajude no meu desenvolvimento e mande uma resposta cabível: "))
 
     frase = '    elif (Frase == "' + Frase + '"):'
-    resposta = '        Resposta == "' + Resposta + '"'
+    resposta = '        Resposta = "' + Resposta + '"'
 
     if (os.path.exists(dir) and os.path.isdir(dir)):
         tipo = 'w'
@@ -51,9 +52,10 @@ def main():
     cta = 0
 
     while (Frase != 'Tchau'):
+        print() #Apenas para melhorar visualização em console
         if (cta == 0):
             print('Olá, eu sou a máquina que busca ser sua amiga! Faça perguntas para mim e se eu souber te responderei. Quando quiser ir embora, basta digitar Tchau.')
-            cta == 1
+            cta = cta + 1
         Frase = str(input('O que você gostaria de dizer: '))
         respostas()
 
@@ -86,31 +88,26 @@ def main():
 
 #Perguntas e respostas armazenadas
 def respostas():
+    global Frase
+    global Resposta
+
     if (Frase == 'Olá, tudo bem?'):
-        Resposta == 'Oii, tudo sim!' + '\n' + 'E você está bem?'
+        Resposta = 'Oii, tudo sim!' + '\n' + 'E você está bem?'
+        print (Resposta)
     elif (Frase == "Qual seu nome?"):
-        Resposta == "Eu não tenho um :("
+        Resposta = "Eu não tenho um :("
+        print (Resposta)
     elif (Frase == "Hello World"):
-        Resposta == "O mundo acena de volta!"
+        Resposta = "O mundo acena de volta!"
+        print(Resposta)
+    elif (Frase == "Oi"):
+        Resposta = "Oi, tudo bem?"
+        print(Resposta)
+
+
     else:
         coletor()
 
+print('MÁQUINA ACUMULADORA DE RESPOSTAS' + '\n' + 'VERSÃO 1.2')
 if (__name__ == '__main__'):
     main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#BANCO DE PERGUNTAS E RESPOSTAS:
